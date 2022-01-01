c72fc848db67d888f45ae1afab8731b723008eaa
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-12-31 17:24:21 -0800

    progress

--- src/App.js
+++ src/App.js
@@ -137,9 +137,6 @@ function formatOperand(operand) {
   return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
-const orange = {
-  backgroundColor:'orange'
-};
 
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -155,28 +152,25 @@ function App() {
         </div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
+      <button className="span-two top" onClick={() => dispatch({ type: ACTIONS.CLEAR })} >
         AC
       </button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+      <button className="top" onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
         DEL
       </button>
-      <OperationButton operation="÷" dispatch={dispatch} style={orange} />
+      <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="*" dispatch={dispatch} className="orange" />
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} className="orange" />
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch={dispatch} className="orange" />
+      <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button
--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,5 +1,5 @@
 import {ACTIONS} from "./App"
 
 export default function OperationButton({dispatch,operation}){
-    return ( <button onClick = {()=>dispatch({type: ACTIONS.CHOOSE_OPERATION, payload:{operation}})}> {operation} </button> )
+    return ( <button style={{backgroundColor: "#FF9500"}} onClick = {()=>dispatch({type: ACTIONS.CHOOSE_OPERATION, payload:{operation}})}> {operation} </button> )
 }
