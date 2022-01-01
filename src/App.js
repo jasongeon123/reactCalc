0ab7d3c4066ec9b5e517fada003e3bba6b833203
Jason Yoo <jasonyoo@Jasons-MBP.lan>
2021-12-31 22:20:05 -0800

    fornow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from "react"
 import { useReducer } from "react"
 import DigitButton from "./DigitButton"
 import OperationButton from "./OperationButton"
-import "./styles.css"
+import styles from "./styles.css"
 
 
 
@@ -137,7 +138,6 @@ function formatOperand(operand) {
   return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
-
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
@@ -152,8 +152,11 @@ function App() {
         </div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
-      <button className="span-two top" onClick={() => dispatch({ type: ACTIONS.CLEAR })} >
+      <button className="top" onClick={() => dispatch({ type: ACTIONS.CLEAR })} >
         AC
+      </button>
+      <button className="top" onClick={() => dispatch({ type: ACTIONS.CLEAR })} >
+        +/-
       </button>
       <button className="top" onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
         DEL
@@ -171,10 +174,10 @@ function App() {
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
       <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton className= {styles.spanTwo} digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button
-        className="span-two"
+        className="spanTwo"
         onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
       >
         =
